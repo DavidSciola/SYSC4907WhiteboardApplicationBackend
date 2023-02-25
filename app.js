2e05a89025a96a4d1ababf91b1f680fb3913de11
DavidSciola <david.sciola1@gmail.com>
2023-02-25 18:15:35 -0500

    test

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ app.use(express.json());
 //var cors = require('cors')
 // app.use(cors())
 
-const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+//testing
+//test
+
+// const cors = require('cors');
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
 
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", '*');
