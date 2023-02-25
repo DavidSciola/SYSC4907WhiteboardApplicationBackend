e794a69c202c0267c54285018d5c2c30d6f5f0fc
DavidSciola <david.sciola1@gmail.com>
2023-02-25 18:52:03 -0500

    test

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ app.use(express.json());
 //var cors = require('cors')
 // app.use(cors())
 
-var cors = require('cors')
-app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// var cors = require('cors')
+// app.use((req, res, next) => {
+//   res.append('Access-Control-Allow-Origin', ['*']);
+//   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.append('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 
 //setup postgres database
