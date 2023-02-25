ac1d04b66573feeee68fbf300c2ea6dd90d4b728
DavidSciola <david.sciola1@gmail.com>
2023-02-25 18:05:42 -0500

    test

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ app.use(express.json());
 //var cors = require('cors')
 // app.use(cors())
 
-//testing
-//test
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 //setup postgres database
 const {Pool} = require("pg");
